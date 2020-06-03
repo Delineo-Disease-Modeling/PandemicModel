40d90bde3f2da6d47b1c72eb4eb4c1fc7232adf3
Madina Bah <bahmadina19@gmail.com>
2020-06-03 15:18:34 -0700

    Updated the schema for the timeseries model, and created a java program that runs through the counties demographic csv file and creates a JSON file

--- model_app/backend/models/Timeseries.js
+++ model_app/backend/models/Timeseries.js
@@ -2,7 +2,46 @@ const mongoose = require("mongoose");
 
 // TODO: fill in actual schema
 const schema = mongoose.Schema({
-  Date: Date
+  infected:{
+    type:Number, 
+    required: true,
+  },
+  CKey:{
+    type:String,
+    required:true,
+  },
+  death:{
+    type:Number,
+    required:true
+  },
+  ">50 gatherings":{
+    type: Boolean,
+    required:true
+  },
+  ">500 gatherings":{
+    type:Boolean,
+    required:true,
+  },
+  "public schools":{
+    type:Boolean,
+    required:true,
+  },
+  "restaurant dine-in":{
+    type:Boolean,
+    required:true
+  },
+  "entertainment/gym":{
+    type:Boolean,
+    required:true,
+  },
+  "federal guidelines":{
+    type:Boolean,
+    required:true,
+  },
+  "foreign travel ban:":{
+    type:Boolean,
+    required:true,
+  }
 });
 
 module.exports = mongoose.model("Timeseries", schema);
