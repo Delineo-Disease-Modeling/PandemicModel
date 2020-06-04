8c6e04ddee1ec68f7018eb6cd17ff8ade0c1153a
Mathias <mathiaswi177@gmail.com>
2020-06-04 00:21:34 -0700

    Cleaned up unused dependencies and added more extensive commenting

--- model_app/backend/server.js
+++ model_app/backend/server.js
@@ -1,27 +1,29 @@
-// dependencies
+/**
+ * Dependencies
+ * mongoose: A mongoDB object modelling library that allows us to manage data 
+ * relationships, provide schema validation, and translate between code objects 
+ * and representations of those objects in mongoDB.
+ * 
+ * express: A Node.js web application server framework.
+ * 
+ * cors: Cross-Origin Resource Sharing. Protocol that allows scripts running on 
+ * browser clinet to interact with resources from a different origin.
+ */
 const mongoose = require('mongoose')
 const express = require('express')
-//const morgan = require('morgan')
-//const errorhandler = require('errorhandler')
-//const bodyParser = require('body-parser') //I think Express versions >= 4.16.0 have this built in
-const routes = require("./routes")
 const cors = require('cors')
 const app = express()
-const port = process.env.PORT || 5000; //Node.js uses port 5000 for dev server
+const port = process.env.PORT || 5000; //Node.js uses port 5000 for development server
 
 require('dotenv').config(); //Allows us to use environment variables from .env file
 const uri = process.env.ATLAS_URI;
 
-//app.use(morgan('dev'))
-//app.use(errorhandler())
 app.use(cors())
 app.use(express.json()) //Let's us parse JSON since server that's server format
-//app.use('/demographics', routes.dem)
-//app.use('/timeseries', routes.time)
 
 mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+  useNewUrlParser: true, //Fall back to old MongoDB connection string parser
+  useUnifiedTopology: true //Opts in to MongoDB's new connection management engine
 });
 
 /**
@@ -32,6 +34,9 @@ connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
+/**
+ * Tell server where to look for demographics and counties routing instructions
+ */
 const demographicsRouter = require('./routes/demographics');
 const countiesRouter = require('./routes/counties');
 
