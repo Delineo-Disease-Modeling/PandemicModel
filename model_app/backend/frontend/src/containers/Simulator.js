34b041489896265bb6d7634df4933426ad9eab09
madinabah2016 <bahmadina19@gmail.com>
2020-07-08 17:10:54 -0700

    Added a few more compoenets to the simulator page, but still not fully complete

--- model_app/backend/frontend/src/containers/Simulator.js
+++ model_app/backend/frontend/src/containers/Simulator.js
@@ -1,7 +1,66 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { County, Timeseries, GoogleMap } from '../components';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import './Simulator.css'
 
+
+function intervention(props){
+
+    return(
+        <div align='left' className='row'>
+
+            <div className='col'>
+                <text style={{color:'white', marginRight:'10px', textAlign:'left'}}>Policy Intervention</text>
+            </div>
+            
+            <div className='col'>
+                <text style={{color:'white', marginRight:'20px'}} >Duration :</text>
+                <text className='durationText' style={{backgroundColor:'#1b4441ad', padding:'5px', textAlign:'center', border:'2px solid #66FCF1', borderRadius:'40px', minWidth:'90px'}}>90-days</text>
+            </div>
+            
+            <div className='col2'>
+                <i align='right' class="fa fa-close" style={{fontSize:'30px', color:'#66FCF1'}}></i>
+            </div>
+            
+
+            <hr align='left' className='dotted'></hr>
+
+        </div>
+    );
+}
+
+class OptionMenu extends Component{
+
+    constructor() {
+        super();
+        this.state = {
+            dropdownOpen:false,
+            selectedPolicy:'Policy'
+        };
+    }
+
+    handleSelectedPolicy = (item)=>{this.setState({selectedPolicy:item})}
+
+    toggle = () => this.setState({dropdownOpen: !this.state.dropdownOpen});
+
+
+    render(){
+        return(
+            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+                <DropdownToggle caret> {this.state.selectedPolicy} </DropdownToggle>
+                <DropdownMenu>
+                    {this.props.list.map((item)=>{
+                        return(
+                        <DropdownItem onClick={()=>{this.setState({selectedPolicy:item})}}>{item}</DropdownItem>
+                        );
+                    })}
+                </DropdownMenu>
+            </Dropdown>
+        );
+        
+    }
+
+}
 
 function Parameters(){
     return(
@@ -18,7 +77,7 @@ function Parameters(){
                                 <label className='label'>Simulation duration (days):</label>
                             </td>
                             <td>
-                                <input required minLength='6' type='text' size='20'></input>
+                                <input required minLength='6' type='text' size='15'></input>
                             </td>
                         </tr>
 
@@ -27,7 +86,7 @@ function Parameters(){
                                 <label className='label'>Other Parameters:</label>
                             </td>
                             <td>
-                                <input required minLength='6' type='text' size='20'></input>
+                                <input required minLength='6' type='text' size='15'></input>
                             </td>
                         </tr>
 
@@ -46,7 +105,7 @@ function Parameters(){
                                 <label className='label'>Infection duration (days):</label>
                             </td>
                             <td>
-                                <input required minLength='6' type='text' size='20'></input>
+                                <input required minLength='6' type='text' size='15'></input>
                             </td>
                         </tr>
 
@@ -55,7 +114,7 @@ function Parameters(){
                                 <label className='label'>Other Parameters:</label>
                             </td>
                             <td>
-                                <input required minLength='6' type='text' size='20'></input>
+                                <input required minLength='6' type='text' size='15'></input>
                             </td>
                         </tr>
 
@@ -69,33 +128,47 @@ function Parameters(){
     );
 }
 
-function Simulator(props) {
-    return(
-        <div className='CardBackground'>
+class Simulator extends Component{
 
-            <div className='GreenBackground'>
-                <h3>Map</h3>
-                <County/>
-                <GoogleMap/> 
+
+    render(){
+
+        return(
+            <div className='CardBackground'>
+
+                <div className='GreenBackground'>
+                    <h3>Map</h3>
+                    <County/>
+                    <GoogleMap/> 
+                </div>
+
+                <div className='GreenBackground'>
+                    <h3>Configurations</h3>
+                    <p style={{textAlign:'left', fontSize:'20px', color:'#66FCF1'}}>Model Parameters</p>
+                    <Parameters/>
+                    <p style={{textAlign:'left', fontSize:'20px', color:'#66FCF1', marginTop:'20px'}}>Intervention Policy</p>
+                    <div style={{textAlign:'left', marginLeft:'55px'}}>
+                        <button className='button'>Add+</button>
+                        
+                    </div>
+                
+                    <br></br>
+                    <button className='button' >Run Simulation</button>
+                </div>
+                
+                <div className='GreenBackground'>
+                    <h3>Analysis</h3>
+                    <Timeseries/>
+                </div>
+                
+                <OptionMenu list={["hello", "hi"]}></OptionMenu>
+
+                
             </div>
+        );
 
-            <div className='GreenBackground'>
-                <h3>Configurations</h3>
-                <p style={{textAlign:'left', fontSize:'20px', color:'#66FCF1'}}>Model Parameters</p>
-                <Parameters/>
-                <p style={{textAlign:'left', fontSize:'20px', color:'#66FCF1', marginTop:'20px'}}>Intervention Policy</p>
-            </div>
-            
-            <div className='GreenBackground'>
-                <h3>Analysis</h3>
-                <Timeseries/>
-            </div>
+    }
 
-
-
-            
-        </div>
-    );
 }
 
 export default Simulator;
