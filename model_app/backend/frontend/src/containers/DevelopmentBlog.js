8cbf0f643063b2e7b39c504079b3f2c3117cfe83
Yixin Pan <airera1999@gmail.com>
2020-07-17 17:47:04 -0500

    dev blog

--- model_app/backend/frontend/src/containers/DevelopmentBlog.js
+++ model_app/backend/frontend/src/containers/DevelopmentBlog.js
@@ -1,8 +1,47 @@
 import React from 'react';
+import './DevelopmentBlog.css'
+
 
 function DevelopmentBlog(props) {
     return(
-        <div className="container"> WIP :)
+        <div class='CardBackground'>
+            <div class='CardGreenBackground'>
+                <h2>Development Blog</h2>
+                <div class='row'>
+                    <div class='column'>
+                         <div class='card1'>
+                            <img class='card-img-top' src="b1.jpg" alt="Card image cap" />
+                            <div class="card-body">
+                                <h5 class="card-title">Sample Blog 1</h5>
+                                <p class="card-text">Short Discription of blog</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                     </div>
+
+                    <div class="column">
+                        <div class='card1'>
+                            <img class='card-img-top' src="b2.png" alt="Card image cap" />
+                            <div class="card-body">
+                            <h5 class="card-title">Sample Blog 2</h5>
+                            <p class="card-text">Short Discription of blog</p>
+                            <p class="card-text"><small class="text-muted">Last updated 1 month ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="column">
+                        <div class='card1'>
+                            <img class='card-img-top' src="b3.jpg" alt="Card image cap" />
+                            <div class="card-body">
+                            <h5 class="card-title">Sample Blog 3</h5>
+                            <p class="card-text">Short Discription of blog</p>
+                            <p class="card-text"><small class="text-muted">Last updated 2 months ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
