548f89913602bb8d2c56947cd529c1f755db63ec
madinabah2016 <bahmadina19@gmail.com>
2020-07-29 14:53:12 -0700

    Edited the team page fromatting

--- model_app/backend/frontend/src/const/leader.js
+++ model_app/backend/frontend/src/const/leader.js
@@ -4,19 +4,11 @@ export const LEADERS = [
     {
       id: 0,
       name: 'Anton Dahbura',
-      image: AntonImg,
       designation: 'Research Supervisor',
       abbr: 'CEO',
       // featured: false,
-      description: "Anton (Tony) Dahbura received the BSEE, MSEE, and PhD in Electrical Engineering and Computer Science from the Johns Hopkins University in 1981, 1982, and 1984, respectively.\n" +
-          "\n" +
-          "From 1983 until 1996 he was a researcher at AT&T Bell Laboratories, was an Invited Lecturer in the Department of Computer Science at Princeton University, and served as Research Director of the Motorola Cambridge Research Center in Cambridge, Massachusetts.\n" +
-          "\n" +
-          "Since 1996 he has led several entrepreneurial efforts in the areas of printing professional baseball operations and commercial real estate.\n" +
-          "\n" +
-          "In January, 2012 he was named Executive Director of the Johns Hopkins University Information Security Institute in Baltimore.\n" +
-          "\n" +
-          "From 2000-2002 he served as Chair of the Johns Hopkins University Engineering Alumni and in 2004 was the recipient of the Johns Hopkins Heritage Award for his service to the University. He chaired The Johns Hopkins Computer Science Department Advisory Board from 1998 until 2012 and also served on the Johns Hopkins University Whiting School of Engineering National Advisory Council during that time."
+      profession: 'Executive Director of Johns Hopkins Infromation Security Insititute',
+      university:'Johns Hopkins University'
     },
 
     {
@@ -24,10 +16,7 @@ export const LEADERS = [
       name: 'Team Leader 1',
       image: ProfileImg,
       designation: 'Team Leader',
-      // abbr: 'CEO',
-      // featured: false,
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."+
-      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+      major:'Team Leader Major'
     },
 
     {
@@ -35,10 +24,29 @@ export const LEADERS = [
       name: 'Team Leader 2',
       image: ProfileImg,
       designation: 'Team Leader',
-      // abbr: 'CEO',
-      // featured: false,
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."+
-      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+      major:'Team Leader Major'
+    },
+    {
+      id: 3,
+      name: 'Team Leader 3',
+      image: ProfileImg,
+      designation: 'Team Leader',
+      major:'Team Leader Major'
+    },
+
+    {
+      id: 3,
+      name: 'Team Leader 3',
+      image: ProfileImg,
+      designation: 'Team Leader',
+      major:'Team Leader Major'
+    },
+    {
+      id: 4,
+      name: 'Team Leader 3',
+      image: ProfileImg,
+      designation: 'Team Leader',
+      major:'Team Leader Major'
     },
 
 ];
