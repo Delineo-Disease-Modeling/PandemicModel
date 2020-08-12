164de837e16e7fa20a1c1a90531e4fd227c4b5f1
emmagan <egan1@jhu.edu>
2020-08-12 12:07:19 -0400

    Fix typos in BarChart

--- model_app/frontend/src/components/BarChart.js
+++ model_app/frontend/src/components/BarChart.js
@@ -10,14 +10,18 @@ class SimpleBarChart extends Component {
     render() {
         const {width, height, data} = this.props;
         return (
-            <BarChart width={width} height={height} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5, }}>
-    <XAxis dataKey="name" interval="preserveStartEnd"/>
+        <BarChart width={width} height={height} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5, }}>
+            <XAxis dataKey="name" interval="preserveStartEnd"/>
             <YAxis interval="preserveStartEnd"/>
             <Tooltip />
             <Legend formatter={this.renderColorfulLegendText}/>
-        <Bar layout="horizontal" dataKey="infected" fill="#8884d8"/>
-            <Bar layout="horizontal" dataKey="deaths" fill="#82ca9d" />
-            </LineChart>
+            <Bar layout="horizontal" dataKey="Susceptible" fill="#8884d8"/>
+            <Bar layout="horizontal" dataKey="Mild" fill="#8884d8"/>
+            <Bar layout="horizontal" dataKey="Severe" fill="#8884d8"/>
+            <Bar layout="horizontal" dataKey="Critical" fill="#8884d8"/>
+            <Bar layout="horizontal" dataKey="Recovered" fill="#8884d8"/>
+            <Bar layout="horizontal" dataKey="Dead" fill="#82ca9d" />
+        </BarChart>
     );
     }
 }
