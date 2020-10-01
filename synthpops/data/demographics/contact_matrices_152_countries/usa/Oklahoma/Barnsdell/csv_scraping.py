09897f50b51cea9ca01fda1ec5b46d4704734587
Shanelle Cao <shanelle.cao@Shanelles-MacBook-Pro.local>
2020-09-30 18:58:41 -0700

    started csv reading pseudocode

---
+++ synthpops/data/demographics/contact_matrices_152_countries/usa/Oklahoma/Barnsdell/csv_scraping.py
@@ -0,0 +1,5 @@
+# Get user input on which data to scrape e.g. age, sex, race etc.
+# Move into the folder and get the file
+# Read csv format and translate table ID using .json file
+# Create a .dat file containing all distribution brackets
+# Format data as [bracket], [percentage] and output to a .dat file
