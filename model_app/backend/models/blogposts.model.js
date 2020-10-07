4b42572e85a90a5597b019efaba388bad4ec4796
Jack Zhang <jackjingyuzhang@outlook.com>
2020-10-07 14:30:20 +0800

    Add blogpost example object

--- model_app/backend/models/blogposts.model.js
+++ model_app/backend/models/blogposts.model.js
@@ -1,16 +1,27 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-  const blogpostsSchema = new Schema({
-    id: Number,
-    title:  String,
-    author: String,
-    body:   String,
-    comments: [{ body: String, author: String, date: Date }],
-    date: { type: Date, default: Date.now },
-  });
+const blogpostsSchema = new Schema({
+  id: {type: Number, required: true},
+  title:  {type: String, required: true},
+  author: {type: String, required: true},
+  body:   {type: String, required: true},
+  comments: [{ body: String, author: String, date: Date }],
+  date: { type: Date, default: Date.now },
+});
 
-  const Post = mongoose.model('Post', blogpostsSchema);
+// Example object for what to return in a query
+// We only select these four fields, plus the ObjectID
+const blogpostObj = {
+	id: 1,
+	title: 'Example Blog Post',
+	author: 'The Backend Team',
+  body: 'Hello, World! This is the body if the example blog post. Bye.',
+};
+
+// Export schema and example object
+module.exports = mongoose.model("Blogposts", blogpostsSchema);
+module.exports.blogpostObj = blogpostObj;
 
 /*
 String category_tag
