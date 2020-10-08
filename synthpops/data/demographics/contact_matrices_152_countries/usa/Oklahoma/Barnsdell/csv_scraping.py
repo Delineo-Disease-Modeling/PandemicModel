f7716fcadf85d93269f45ac240774b5e04447228
Shanelle Cao <shanelle.cao@Shanelles-MacBook-Pro.local>
2020-10-07 18:34:10 -0700

    update identifiers

--- synthpops/data/demographics/contact_matrices_152_countries/usa/Oklahoma/Barnsdell/csv_scraping.py
+++ synthpops/data/demographics/contact_matrices_152_countries/usa/Oklahoma/Barnsdell/csv_scraping.py
@@ -4,18 +4,4 @@
 # Create a .dat file containing all distribution brackets
 # Format data as [bracket], [percentage] and output to a .dat file
 
-import csv
-import json
-
-type = input("Demographic type: ")
-if type == "age" or type == "sex":
-    csv = "age_sex_distr.csv"
-    json = "age_sex_metada.json"
-elif type == "race":
-    csv = "race_distr.csv"
-    json = "race_metadata.json"
-elif type == "income":
-    csv = "income_distr.csv"
-    json = "income_metadata.json"
-
-# Read csv file based on dictionary
+import pandas as pd
