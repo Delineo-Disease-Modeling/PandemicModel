3ccdcee6f0f26e9b4e3f83c62526e59612f97715
GWCMelody <melody2011102@gmail.com>
2020-10-11 14:30:14 +0800

    Moved GridList to another file

--- model_app/frontend/src/components/TeamGridList.js
+++ model_app/frontend/src/components/TeamGridList.js
@@ -1,50 +1,6 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import IconButton from '@material-ui/core/IconButton';
-import InfoIcon from '@material-ui/icons/Info';
+import GridList from './GridList';
 import tileData from '../const/teamTileData.js';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: '#1b4441c2',
-  },
-  gridList: {
-    width: 800
-  },
-  icon: {
-    color: 'rgba(255, 255, 255, 0.54)',
-  },
-}));
-
-export default function TitlebarGridList() {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.root}>
-      <GridList cellWidth={200} className={classes.gridList} cols={4}>
-        {tileData.map((tile) => (
-          <GridListTile key={tile.img}>
-            <img src={tile.img} alt={tile.name} />
-            <GridListTileBar
-              title={tile.name}
-              subtitle={<span>{tile.role}</span>}
-              actionIcon={
-                <IconButton aria-label={`info about ${tile.name}`} className={classes.icon}>
-                  <InfoIcon />
-                </IconButton>
-              }
-            />
-          </GridListTile>
-        ))}
-      </GridList>
-    </div>
-  );
+export default function TeamGridList() {
+  return GridList(tileData);
 }
