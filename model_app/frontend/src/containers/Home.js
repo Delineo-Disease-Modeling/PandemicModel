a4bc1375326b0f72732eaf658750fc211fac3082
GWCMelody <melody2011102@gmail.com>
2020-10-19 01:17:14 +0800

    updated Home page

--- model_app/frontend/src/containers/Home.js
+++ model_app/frontend/src/containers/Home.js
@@ -2,35 +2,37 @@ import React, { Component } from 'react';
 import { Media } from 'reactstrap';
 import { AboutImg, DevelopmentBlogImg, SimulatorImg, TeamImg } from '../images'
 
-class Home extends Component{
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
+class Home extends Component {
     constructor(props) {
         super(props);
-
         this.links = [
             {
                 id: 0,
-                name:'About',
+                name: 'About',
                 image: AboutImg,
-                description:'Overview of our Covid-19 Modeling project'
+                description: 'Overview of our Covid-19 Modeling project'
             },
             {
                 id: 1,
-                name:'Team',
+                name: 'Team',
                 image: TeamImg,
-                description:'Our team composition and leadership'
+                description: 'Our team composition and leadership'
             },
             {
                 id: 2,
-                name:'Simulator',
+                name: 'Simulator',
                 image: SimulatorImg,
-                description:'Use this page to run the simulator we create for Covid-19 Modeling'
+                description: 'Use this page to run the simulator we create for Covid-19 Modeling'
             },
             {
                 id: 3,
-                name:'Development Blog',
+                name: 'Development Blog',
                 image: DevelopmentBlogImg,
-                description:'The page displaying our process of simulator development and expectation on future update'
+                description: 'The page displaying our process of simulator development and expectation on future update'
             },
         ];
     }
@@ -38,8 +40,10 @@ class Home extends Component{
     render() {
 
         const menu = this.links.map((link) => {
-            let imagStyle = {maxHeight: 128,
-                maxWidth: 128};
+            let imagStyle = {
+                maxHeight: 128,
+                maxWidth: 128
+            };
             return (
                 <div key={link.id} className="col-12 mt-5">
                     <Media tag="li">
@@ -47,7 +51,7 @@ class Home extends Component{
                             <Media object src={link.image} style={imagStyle} alt={link.name} />
                         </Media>
                         <Media body className="ml-5">
-                            <Media heading>{link.name}</Media>
+                            <Media heading style={{color: "#66FCF1"}}>{link.name}</Media>
                             <p>{link.description}</p>
                         </Media>
                     </Media>
@@ -56,13 +60,18 @@ class Home extends Component{
         });
 
         return (
-            <div className="container">
-                <div className="row">
-                    <Media list>
-                        {menu}
-                    </Media>
-                </div>
-            </div>
+            <React.Fragment>
+                <CssBaseline />
+                <Container maxWidth="xl">
+                    <Typography component="div" style={{ margin: '-30px', backgroundColor: 'black', backgroundImage: "url('https://9to5mac.com/wp-content/uploads/sites/6/2020/01/Coronavirus.jpg?quality=82&strip=all')", backgroundRepeat: "no-repeat", height: '100vh' }} />
+
+                    <div className="row" style={{position: "absolute", top: '13%', left: "10%", backgroundColor: "rgba(0, 0, 0, 0.70)"}}>
+                            <Media list>
+                                {menu}
+                            </Media>
+                        </div>
+                </Container>
+            </React.Fragment>
         );
     }
 }
