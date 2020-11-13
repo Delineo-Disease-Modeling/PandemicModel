6fa7c02422d6b4179846be696ad0c496178b0980
Emerson <esulli26@jhu.edu>
2020-11-13 15:30:00 -0500

    Created Skeleton Submodule class for MBDS model. Added to simulation folder.

---
+++ simulation/submodule.py
@@ -0,0 +1,35 @@
+class Submodule:
+
+    def __init__(self, Area, Contact, Mobility, Density, Cleanliness, numGroups, Groups):
+        # Either initialize parameterized or empty and fill in with methods.
+        self.__Area = Area
+        self.__Contact = Contact
+        self.__Mobility = Mobility
+        self.__Density = Density
+        self.__Cleanliness = Cleanliness
+        self.__numGroups = numGroups
+        self.__Groups = Groups
+
+    def calcContact(self):
+        contact = 0 #placeholder
+        return contact
+
+    def calcMobility(self):
+        mobility = 0 #placeholder
+        return mobility
+
+    def calcDensity(self):
+        density = 0 #placeholder
+        return density
+    def calcNumGroups(self):
+        numGroups = 0 #placeholder
+        return numGroups
+
+    def createGroups(self):
+        groups = {} #placeholder
+        return groups
+
+    def calcCleanliness(self):
+        cleanliness = 0 #placeholder
+        return cleanliness
+
