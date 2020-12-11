f1329727a44b68f0bcccbf82f3ab6daf89760fa3
Emerson <esulli26@jhu.edu>
2020-12-11 14:41:17 -0800

    Changes to mdbs model

---
+++ simulation/module.py
@@ -0,0 +1,16 @@
+class Module:
+
+    def __init__(self, State, County, Interventions):
+        self.__State = State
+        self.__County = County
+
+    def createPopulation(self):
+
+    # TODO
+
+    def createSubmodules(self):
+
+    # TODO
+
+    def movePop(self, population, submodules):
+# TODO
