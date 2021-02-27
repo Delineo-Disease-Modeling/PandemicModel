20a9f027865ad499e940feeae9e8684dd79f6958
Jack Zhang <jackjingyuzhang@outlook.com>
2021-02-28 00:02:38 +0800

    Refines alignment and makes the text look nicer

--- model_app/frontend/src/containers/Home.js
+++ model_app/frontend/src/containers/Home.js
@@ -4,6 +4,7 @@ import Grid from '@material-ui/core/Grid';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core';
+import { palette } from '@material-ui/system';
 
 const styles = {
     button: {
@@ -48,7 +49,7 @@ const styles = {
     },
 
     aboutSection: {
-        padding: '100px 10px 100px 10px',
+        padding: '100px 100px 100px 100px',
         backgroundColor: '#222D3C', /* Or #1C2531 ? */
         overflow: 'hidden',
 
@@ -147,7 +148,7 @@ class Home extends Component {
                             <Typography variant="h5" component="h3" gutterBottom>
                                 Description of ML
                                 </Typography>
-                            <p style={{ fontSize: '18px' }}>
+                            <p style={{ fontSize: '18px', color: 'lightgrey' }}>
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. In arcu ligula, auctor ut dictum ac, malesuada et arcu. Vestibulum ut commodo enim, in pulvinar magna. Mauris est enim, pretium id porta ut, maximus at felis. Donec aliquam, velit a tempus accumsan, lorem metus viverra augue, ac convallis quam enim quis lacus. Aliquam ipsum orci, ullamcorper vel erat in, semper posuere tortor. Vestibulum consequat ante nec mauris iaculis ultrices. Vivamus non sagittis sem. Duis a laoreet ante, in facilisis nisl. Quisque tempor non orci id accumsan. Ut nulla augue, ultricies vel odio sed, porta finibus massa. Nunc auctor ante sit amet mi fermentum accumsan. Phasellus fermentum sed lacus id ornare. Curabitur congue, neque sed porta faucibus, risus orci aliquet ex, id fermentum nisl sapien vel sem. Nunc sodales, elit et hendrerit consectetur, sem mauris tempus ligula, sed faucibus quam libero a nibh. Sed orci ex, imperdiet dignissim urna ac, egestas bibendum tortor.
                                 </p>
                             <div className={classes.buttonContainer}>
@@ -161,7 +162,7 @@ class Home extends Component {
                             <Typography variant="h5" component="h3" gutterBottom>
                                 Description of Simulator
                             </Typography>
-                            <p style={{ fontSize: '18px' }}>
+                            <p style={{ fontSize: '18px', color: 'lightgrey' }}>
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. In arcu ligula, auctor ut dictum ac, malesuada et arcu. Vestibulum ut commodo enim, in pulvinar magna. Mauris est enim, pretium id porta ut, maximus at felis. Donec aliquam, velit a tempus accumsan, lorem metus viverra augue, ac convallis quam enim quis lacus. Aliquam ipsum orci, ullamcorper vel erat in, semper posuere tortor. Vestibulum consequat ante nec mauris iaculis ultrices. Vivamus non sagittis sem. Duis a laoreet ante, in facilisis nisl. Quisque tempor non orci id accumsan. Ut nulla augue, ultricies vel odio sed, porta finibus massa. Nunc auctor ante sit amet mi fermentum accumsan. Phasellus fermentum sed lacus id ornare. Curabitur congue, neque sed porta faucibus, risus orci aliquet ex, id fermentum nisl sapien vel sem. Nunc sodales, elit et hendrerit consectetur, sem mauris tempus ligula, sed faucibus quam libero a nibh. Sed orci ex, imperdiet dignissim urna ac, egestas bibendum tortor.
                             </p>
                             <div className={classes.buttonContainer}>
@@ -200,7 +201,7 @@ class Home extends Component {
                                             </Typography>
                                         </Grid>
                                     </Grid>
-                                    <p className={classes.blogText}>
+                                    <p className={classes.blogText} style={{ fontSize: '18px', color: 'lightgrey' }}>
                                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. In arcu ligula, auctor ut dictum ac, malesuada et arcu. Vestibulum ut commodo enim, in pulvinar magna. Mauris est enim, pretium id porta ut, maximus at felis. Donec aliquam, velit a tempus accumsan, lorem metus viverra augue, ac convallis quam enim quis lacus.
                                     </p>
                                     <div className={classes.buttonContainer}>
@@ -229,7 +230,7 @@ class Home extends Component {
                                             </Typography>
                                         </Grid>
                                     </Grid>
-                                    <p className={classes.blogText}>
+                                    <p className={classes.blogText} style={{ fontSize: '18px', color: 'lightgrey' }}>
                                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. In arcu ligula, auctor ut dictum ac, malesuada et arcu. Vestibulum ut commodo enim, in pulvinar magna. Mauris est enim, pretium id porta ut, maximus at felis. Donec aliquam, velit a tempus accumsan, lorem metus viverra augue, ac convallis quam enim quis lacus.
                                     </p>
                                     <div className={classes.buttonContainer}>
@@ -258,7 +259,7 @@ class Home extends Component {
                                             </Typography>
                                         </Grid>
                                     </Grid>
-                                    <p className={classes.blogText}>
+                                    <p className={classes.blogText} style={{ fontSize: '18px', color: 'lightgrey' }}>
                                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. In arcu ligula, auctor ut dictum ac, malesuada et arcu. Vestibulum ut commodo enim, in pulvinar magna. Mauris est enim, pretium id porta ut, maximus at felis. Donec aliquam, velit a tempus accumsan, lorem metus viverra augue, ac convallis quam enim quis lacus.
                                     </p>
                                     <div className={classes.buttonContainer}>
