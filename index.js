320853a45ecdc946ae6cdbe92360430a73962495
Sebastian Cabrejos <Boccca2014@gmail.com>
2021-03-17 19:26:18 -0400

    added index.js file

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js file
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Express!");
+});
+
+app.listen(port, () => {
+  console.log(`Express app listening at http://localhost:${port}`);
+});
