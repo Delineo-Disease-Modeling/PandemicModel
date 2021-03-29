70037799ab9641ceebc35b8eb61ebdcb03f1ca74
Sebastian Cabrejos <Boccca2014@gmail.com>
2021-03-29 12:52:04 -0400

    Server-to-Server Connection (#64)
    * update branch with original repo
    
    * added index.js file
    
    * fixed folder
    
    * created express app
    
    * added temp json file to view and some instructions
    
    * Added index.js and a test program
    
    * servers interact, can send json and py files
    
    * added some notes for next steps
    
    * small note in test py
    
    * mchanged implementation for passing data, added new requests
    
    * added comments, notes, and async function
    
    * added comments, notes, and async function
    
    * aadded some logic for json passing
    
    * properly passing JSON to python file, made some adjustments to passing JSON files
    
    * added note to allow python to parse JSON
    
    * updated gitignore
    
    * reversed changed to gitignore
    
    * Delete package-lock.json
    
    Co-authored-by: Mehul <mehul7169@gmail.com>
    Co-authored-by: Delineo Disease Modeling <66046047+DelineoModeling@users.noreply.github.com>

---
+++ servers/simulator.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const simulator = express();
+const jsonData = require("./data.json");
+const http = require("http");
+const fs = require("fs");
+
+simulator.use(express.json());
+
+// returns post request to website then read by other server
+simulator.post("/simulator", (req, res) => {
+  console.log(req.body);
+  const { spawn } = require("child_process");
+  const pyProg = spawn("python3", ["./test.py", req.body]);
+  pyProg.stdout.on("data", function (data) {
+    console.log(data);
+    res.write(data);
+    res.end("end");
+  });
+
+  res.json(jsonData); // send new JSON file
+
+  // read newly generated json file
+  let data = ""; 
+  fs.readFile("data.json", async (e, data) => {
+    try {
+      data = await JSON.parse(data);
+      console.log(JSON.stringify(data));
+    } catch (e) {
+      throw e;
+    }
+  });
+
+  // gets post request from other server
+  var options = {
+    host: "localhost",
+    port: 3000,
+    path: "/",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+  };
+  // parses data from other server
+  var httpreq = http.request(options, function (response) {
+    response.setEncoding("utf8");
+    response.on("data", function (chunk) {
+      console.log("body: " + chunk);
+    });
+    response.on("end", function () {
+      res.json(JSON.stringify(jsonData));
+    });
+  });
+
+  // handle error on failed POST
+  httpreq.on("error", (e) => {
+    console.error(`problem with request: ${e.message}`);
+  });
+
+  httpreq.write(data);
+  httpreq.end();
+});
+
+// listen on port 4000
+simulator.listen(4000, () =>
+  console.log("Application listening on port 4000!")
+);
