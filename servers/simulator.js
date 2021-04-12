ab5eb1257b74af7da22be0732121ce818929623e
Simone <70237983+s-bliss@users.noreply.github.com>
2021-04-12 17:24:04 -0400

    https set up (#75)
    Co-authored-by: Simone Bliss <sea.monester99@gmail.com>

--- servers/simulator.js
+++ servers/simulator.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const simulator = express();
-const http = require("http");
+const https = require("https");
 const port = 22; // process.env.PORT || 
 // const jsonData = require("./data.json");
 // const fs = require("fs");
+
+var privateKey  = fs.readFileSync('/etc/pki/tls/certs/covidmod.isi.jhu.edu.2021.cer', 'utf8');
+var certificate = fs.readFileSync('/etc/pki/tls/private/covidmod.isi.jhu.edu.key', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+var httpsServer = https.createServer(credentials, app);
 
 simulator.use(express.json());
 
@@ -25,8 +30,8 @@ simulator.post("/", (req, res) => {
 
   // gets post request from other server
   var options = {
-    host: "covidweb.local",
-    port: 22,
+    host: "covidweb.isi.jhu.edu",
+    port: 443,
     path: "/",
     method: "POST",
     headers: {
@@ -35,7 +40,7 @@ simulator.post("/", (req, res) => {
     },
   };
   // parses data from other server
-  var httpreq = http.request(options, function (response) {
+  var httpsreq = https.request(options, function (response) {
     response.setEncoding("utf8");
     response.on("data", function (chunk) {
       console.log("body: " + chunk);
@@ -46,15 +51,15 @@ simulator.post("/", (req, res) => {
   });
 
   // handle error on failed POST
-  httpreq.on("error", (e) => {
+  httpsreq.on("error", (e) => {
     console.error(`problem with request: ${e.message}`);
   });
 
-  httpreq.write(data);
-  httpreq.end();
+  httpsreq.write(data);
+  httpsreq.end();
 });
 
-// listen on port 22
-simulator.listen(port, () =>
+// listen on port 443
+httpsServer.listen(port, () =>
   console.log(`Application listening on port ${port}!`)
 );
