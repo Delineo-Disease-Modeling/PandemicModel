1099f419fd5f9d2a80c6f43716c39cdabf853a96
Elaine Wang <elainew@Elaines-MacBook-Pro-2.local>
2021-06-24 14:06:29 -0400

    ave, var, stdev added

--- simulation/testscript.py
+++ simulation/testscript.py
@@ -1,6 +1,8 @@
 #Test Script to run sim
 import matplotlib.pyplot as plt
 from master import MasterController as mc
+import statistics
+import numpy as np
 
 
 numRuns = int(input("enter number of runs: "))
@@ -37,6 +39,21 @@ plt.xlabel('hours')
 plt.ylabel('total infected')
 plt.title('Test Run for Total Infected')
 plt.show()
+#runsTotal = np.array(runsTotal)
+# ave, var, st dev calculations
+ave_runsTotal = []
+var_runsTotal = []
+stdev_runsTotal = []
+for i in range(len(runsTotal)):
+    ave_runsTotal.append(statistics.mean(runsTotal[i]))
+    var_runsTotal.append(statistics.pvariance(runsTotal[i], ave_runsTotal[i]))
+    stdev_runsTotal.append(statistics.pstdev(runsTotal[i]))
+print('Total infections statstics:')
+print('Average:', ave_runsTotal)
+print('Variance:', var_runsTotal)
+print('Standard deviation:', stdev_runsTotal)
+
+
 
 plt.figure()
 #Facilities graph
@@ -45,7 +62,21 @@ for i in range(len(runsFacilities)):
 plt.xlabel('hours')
 plt.ylabel('infected in Facilities')
 plt.title('Test Run for Infected in Facilities')
-plt.show()
+plt.show() 
+# ave, var, st dev calculations
+ave_runsFacilities = []
+var_runsFacilities = []
+stdev_runsFacilities = []
+for i in range(len(runsFacilities)):
+    ave_runsFacilities.append(statistics.mean(runsFacilities[i]))
+    var_runsFacilities.append(statistics.pvariance(runsFacilities[i], ave_runsFacilities[i]))
+    stdev_runsFacilities.append(statistics.pstdev(runsFacilities[i]))
+print('Infections in facilities statistics:')
+print('Average:', ave_runsFacilities)
+print('Variance:', var_runsFacilities)
+print('Standard deviation:', stdev_runsFacilities)
+
+
 
 plt.figure()
 #Households Graph
@@ -55,6 +86,20 @@ plt.xlabel('hours')
 plt.ylabel('infected in households')
 plt.title('Test Run for Infected in Households')
 plt.show()
+# ave, var, st dev calculations
+ave_runsHouses = []
+var_runsHouses = []
+stdev_runsHouses = []
+for i in range(len(runsHouses)):
+    ave_runsHouses.append(statistics.mean(runsHouses[i]))
+    var_runsHouses.append(statistics.pvariance(runsHouses[i], ave_runsHouses[i]))
+    stdev_runsHouses.append(statistics.pstdev(runsHouses[i]))
+print('Infection in households statistics:')
+print('Average:', ave_runsHouses)
+print('Variance:', var_runsHouses)
+print('Standard deviation:', stdev_runsHouses)
+
+
 
 
 runsPeople = currRun.peopleInFacilities
